d31a2371c5878d753c27f7d946f92c7cc82526fb
saba-tandashvili <sb.tandashvili@gmail.com>
2024-02-19 23:30:36 +0400

    commit

--- day 2/homework/homework.py
+++ day 2/homework/homework.py
@@ -1,1 +1,8 @@
-print("hello")
+Levani = 40
+Gio = 13
+Tiko = 38
+Saba = 13
+Tamila = 71
+Lia = 65
+
+print("Levani =", Levani + 20, "Gio =", Gio + 20, "Tiko =", Tiko + 20, "Saba =", Saba + 20, "Tamila =", Tamila + 20, "Lia =", Lia + 20)
