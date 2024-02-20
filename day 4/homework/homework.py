525553e8f60d422fe341feb5d9416a1e0baaf604
saba-tandashvili <sb.tandashvili@gmail.com>
2024-02-20 22:55:31 +0400

    commit

---
+++ day 4/homework/homework.py
@@ -0,0 +1,7 @@
+salary = int(input("What is your salary?: "))
+if salary >= 10000:
+    print("Did you learn in GOA?")
+elif salary >= 1000 and salary <= 10000:
+    print("You mid")
+else:
+    print("Dang bro you should've joined GOA")
