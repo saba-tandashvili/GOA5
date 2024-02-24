4ffb43be7976e0c3e030331af6f3b0ec80794066
saba-tandashvili <sb.tandashvili@gmail.com>
2024-02-24 23:29:10 +0400

    commit

---
+++ day 6/homework/homework.py
@@ -0,0 +1,12 @@
+ticket = 25
+children_free = 0
+
+children_free = ticket - children_free
+
+ticket_adults = 10
+ticket_children = 3
+total_tickets = 0
+
+total_tickets = (ticket_adults * ticket_adults) + (ticket_children * ticket_children)
+
+print(total_tickets)
